799783aa410410ddc673d9aef8e017bf3732ee5b
paulo diaz <paulodiaz@192.168.1.6>
2022-12-12 22:16:49 -0600

    deleted modules

--- pages/leaderboardz/index.tsx
+++ pages/leaderboardz/index.tsx
@@ -1,27 +1,13 @@
-import { Container, Typography, Box, Stack } from "@mui/material";
-import { flexbox } from "@mui/system";
+import { Typography } from "@mui/material";
 
 
 export default function Leaderboardz() {
   
   return (
-    <Container>
-      <Typography variant="h4" align="center" gutterBottom>Leaderboardz</Typography>
-      <Typography variant="subtitle1" align="center">Statics</Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Stack paddingX={4} paddingY={1} marginX={2} sx={{ border: '1px solid  #011335', borderRadius: '1rem' }}>
-          <Typography align="center">Collections</Typography>
-          <Typography align="center">5</Typography>
-        </Stack>
-        <Stack paddingX={4} paddingY={1} marginX={2} sx={{ border: '1px solid  #011335', borderRadius: '1rem' }}>
-          <Typography align="center">Collections</Typography>
-          <Typography align="center">5</Typography>
-        </Stack>
-        <Stack paddingX={4} paddingY={1} marginX={2} sx={{ border: '1px solid  #011335', borderRadius: '1rem' }}>
-          <Typography align="center">Collections</Typography>
-          <Typography align="center">5</Typography>
-        </Stack>
-      </Box>
-    </Container>
+    <>
+      <Typography variant="h1">
+        Leaderboardz
+      </Typography>
+    </>
   );
 }
