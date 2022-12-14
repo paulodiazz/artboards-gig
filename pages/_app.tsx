f5d852428b5b8622960bb3cabb9726c73f0635ab
paulo diaz <paulodiaz@192.168.1.6>
2022-12-14 16:37:32 -0600

    admin update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,11 +15,11 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { ListItemButton, ListItem, Link, Avatar, Menu, Tooltip, MenuItem } from '@mui/material';
+import { ListItemButton, ListItem, Link, Avatar, Menu, Tooltip, MenuItem, ListItemIcon } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Button from '@mui/material/Button/Button';
-import { MenuProps } from '@mui/material/Menu';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import StyleIcon from '@mui/icons-material/Style';
+import { ThemeProvider } from "@mui/material/styles";
 
 const drawerWidth = 240;
 const settings = ['Profile', 'Logout'];
@@ -106,135 +106,124 @@ export default function App({ Component, pageProps }: AppProps) {
   };
 	
 	return (
-		<MeshProvider>
-			<Box sx={{ display: 'flex' }}>
-      	<CssBaseline />
-				<AppBar position="fixed" open={open}>
-					<Toolbar>
-						<IconButton
-							color="inherit"
-							aria-label="open drawer"
-							onClick={handleDrawerOpen}
-							edge="start"
-							sx={{ mr: 2, ...(open && { display: 'none' }) }}
-						>
-							<MenuIcon />
-						</IconButton>
-						<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-							
-						</Typography>
-						<Box sx={{ flexGrow: 0 }}>
-								<Button
-									id="demo-customized-button"
-									aria-controls={openCardano ? 'demo-customized-menu' : undefined}
-									aria-haspopup="true"
-									aria-expanded={openCardano ? 'true' : undefined}
-									variant="contained"
-									disableElevation
-									onClick={handleClick}
-									startIcon={<KeyboardArrowDownIcon />}
-								>
-									650 ADA
-								</Button>
-								<Menu
-									id="demo-customized-menu"
-									MenuListProps={{
-										'aria-labelledby': 'demo-customized-button',
-									}}
-									anchorEl={anchorEl}
-									open={openCardano}
-									onClose={handleClose}
-								>
-									<MenuItem onClick={handleClose} disableRipple>
-										Edit
-									</MenuItem>
-									<MenuItem onClick={handleClose} disableRipple>
-										Duplicate
-									</MenuItem>
-									<Divider sx={{ my: 0.5 }} />
-									<MenuItem onClick={handleClose} disableRipple>
-										Archive
-									</MenuItem>
-									<MenuItem onClick={handleClose} disableRipple>
-										More
-									</MenuItem>
-								</Menu>
-							<Tooltip title="Open settings">
-								<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-									<Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-								</IconButton>
-							</Tooltip>
-							<Menu
-								sx={{ mt: '45px' }}
-								id="menu-appbar"
-								anchorEl={anchorElUser}
-								anchorOrigin={{
-									vertical: 'top',
-									horizontal: 'right',
-								}}
-								keepMounted
-								transformOrigin={{
-									vertical: 'top',
-									horizontal: 'right',
-								}}
-								open={Boolean(anchorElUser)}
-								onClose={handleCloseUserMenu}
-							>
-								{settings.map((setting) => (
-									<MenuItem key={setting} onClick={handleCloseUserMenu}>
-										<Typography textAlign="center">{setting}</Typography>
-									</MenuItem>
-								))}
-							</Menu>
-						</Box>
-					</Toolbar>
-				</AppBar>
-				<Drawer
-					sx={{
-						width: drawerWidth,
-						flexShrink: 0,
-						'& .MuiDrawer-paper': {
-							width: drawerWidth,
-							boxSizing: 'border-box',
-						},
-					}}
-					variant="persistent"
-					anchor="left"
-					open={open}
-				>
-					<DrawerHeader>
-						<IconButton onClick={handleDrawerClose}>
-							{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-						</IconButton>
-					</DrawerHeader>
-					<List>
-						<ListItem disablePadding>
-							<ListItemButton>
-								<Link href={'/home'} color='inherit' underline='none' variant='subtitle1'>Home</Link>
-							</ListItemButton>
-						</ListItem>
-						<ListItem disablePadding>
-							<ListItemButton>
-								<Link href={'/releases'} color='inherit' underline='none' variant='subtitle1'>New Releases</Link>
-							</ListItemButton>
-						</ListItem>
-						<ListItem disablePadding>
-							<ListItemButton>
-								<Link href={'/artboardz'} color='inherit' underline='none' variant='subtitle1'>Artboardz</Link>
-							</ListItemButton>
-						</ListItem>
-						<ListItem disablePadding>
-							<ListItemButton>
-								<Link href={'/leaderboardz'} color='inherit' underline='none' variant='subtitle1'>Leaderboardz </Link>
-							</ListItemButton>
-						</ListItem>
-					</List>
-				</Drawer>
-				<Main sx={{ background: 'radial-gradient(75.93% 75.93% at 13.92% 23.07%, #0056FF 0%, #FF3800 100%)' }} open={open}>
-					<DrawerHeader />
-					<Component {...pageProps} />
-				</Main>
-			</Box>
-		</MeshProvider>
+    <ThemeProvider theme={theme}>
+      <MeshProvider>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open} sx={{ background: '#14171F' }}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: 'none' }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+              </Typography>
+              <Box sx={{ flexGrow: 0 }}>
+                  <Menu
+                    id="demo-customized-menu"
+                    MenuListProps={{
+                      'aria-labelledby': 'demo-customized-button',
+                    }}
+                    anchorEl={anchorEl}
+                    open={openCardano}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose} disableRipple>
+                      Edit
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} disableRipple>
+                      Duplicate
+                    </MenuItem>
+                    <Divider sx={{ my: 0.5 }} />
+                    <MenuItem onClick={handleClose} disableRipple>
+                      Archive
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} disableRipple>
+                      More
+                    </MenuItem>
+                  </Menu>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+                backgroundColor: '#14171F',
+                color: '#FEFEFE'
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <Typography variant='h5' align='left'>Admin Panel</Typography>
+              <IconButton sx={{ display: { md: 'none' } }} onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon sx={{ color: 'white' }}/> : <ChevronRightIcon sx={{ color: 'white' }}/>}
+              </IconButton>
+            </DrawerHeader>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <PeopleOutlineIcon sx={{ color: 'white' }}/>
+                  </ListItemIcon>
+                  <Link href={'/admin/users'} color='inherit' underline='none' variant='subtitle1'>Collectors</Link>
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <StyleIcon sx={{ color: 'white' }}/>
+                  </ListItemIcon>
+                  <Link href={'/admin/collections'} color='inherit' underline='none' variant='subtitle1'>Collections</Link>
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Drawer>
+          <Main open={open}>
+            <DrawerHeader />
+            <Component {...pageProps} />
+          </Main>
+        </Box>
+      </MeshProvider>
+    </ThemeProvider>
+		
 	)
 }
